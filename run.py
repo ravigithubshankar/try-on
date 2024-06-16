34a65faa0736ac53620bba0b4ebcbc530daf7da4
Ravi <99581144+ravigithubshankar@users.noreply.github.com>
2024-06-16 09:13:45 +0530

    Update run.py

--- run.py
+++ run.py
@@ -10,32 +10,32 @@ def resize_img(path):
     im.save(path)
 
 
-for path in os.listdir('/content/inputs/test/cloth/'):
-    resize_img(f'/content/inputs/test/cloth/{path}')
+for path in os.listdir('/tmp/inputs/test/cloth/'):
+    resize_img(f'/tmp/inputs/test/cloth/{path}')
 
-os.chdir('/content/clothes-virtual-try-on')
-os.system("rm -rf /content/inputs/test/cloth/.ipynb_checkpoints")
+os.chdir('/tmp/clothes-virtual-try-on')
+os.system("rm -rf /tmp/inputs/test/cloth/.ipynb_checkpoints")
 os.system("python cloth-mask.py")
 os.chdir('/content')
-os.system("python /content/clothes-virtual-try-on/remove_bg.py")
+os.system("python /app/clothes-virtual-try-on/remove_bg.py")
 os.system(
-    "python3 /content/Self-Correction-Human-Parsing/simple_extractor.py --dataset 'lip' --model-restore '/content/Self-Correction-Human-Parsing/checkpoints/final.pth' --input-dir '/content/inputs/test/image' --output-dir '/content/inputs/test/image-parse'")
+    "python3 /app/Self-Correction-Human-Parsing/simple_extractor.py --dataset 'lip' --model-restore '/app/Self-Correction-Human-Parsing/checkpoints/final.pth' --input-dir '/tmp/inputs/test/image' --output-dir '/tmp/inputs/test/image-parse'")
 os.chdir('/content')
 os.system(
-    "cd openpose && ./build/examples/openpose/openpose.bin --image_dir /content/inputs/test/image/ --write_json /content/inputs/test/openpose-json/ --display 0 --render_pose 0 --hand")
+    "cd openpose && ./build/examples/openpose/openpose.bin --image_dir /tmp/inputs/test/image/ --write_json /tmp/inputs/test/openpose-json/ --display 0 --render_pose 0 --hand")
 os.system(
-    "cd openpose && ./build/examples/openpose/openpose.bin --image_dir /content/inputs/test/image/ --display 0 --write_images /content/inputs/test/openpose-img/ --hand --render_pose 1 --disable_blending true")
+    "cd openpose && ./build/examples/openpose/openpose.bin --image_dir /tmp/inputs/test/image/ --display 0 --write_images /tmp/inputs/test/openpose-img/ --hand --render_pose 1 --disable_blending true")
 
-model_image = os.listdir('/content/inputs/test/image')
-cloth_image = os.listdir('/content/inputs/test/cloth')
+model_image = os.listdir('/tmp/inputs/test/image')
+cloth_image = os.listdir('/tmp/inputs/test/cloth')
 pairs = zip(model_image, cloth_image)
 
-with open('/content/inputs/test_pairs.txt', 'w') as file:
+with open('/tmp/inputs/test_pairs.txt', 'w') as file:
     for model, cloth in pairs:
         file.write(f"{model} {cloth}")
 
 # making predictions
 os.system(
-    "python /content/clothes-virtual-try-on/test.py --name output --dataset_dir /content/inputs --checkpoint_dir /content/clothes-virtual-try-on/checkpoints --save_dir /content/")
-os.system("rm -rf /content/inputs")
-os.system("rm -rf /content/output/.ipynb_checkpoints")
+    "python /app/clothes-virtual-try-on/test.py --name output --dataset_dir /tmp/inputs --checkpoint_dir /app/clothes-virtual-try-on/checkpoints --save_dir /tmp/")
+os.system("rm -rf /tmp/inputs")
+os.system("rm -rf /tmp/output/.ipynb_checkpoints")
